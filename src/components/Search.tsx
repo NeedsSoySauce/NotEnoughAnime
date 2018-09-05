0b94f1c84315bfca8ada11f184d118defa60109f
Feras Albaroudi <feras.albaroudi@gmail.com>
2018-09-05 12:52:04 +1200

    Added input validation to text input and converted input to URL format before submitting the search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,36 +2,58 @@ import { Button, TextField } from '@material-ui/core/';
 import SearchIcon from '@material-ui/icons/Search';
 import * as React from 'react';
 
-/* function SearchButton(props: any) {
-    return (
-        <Button onClick={props.handleClick}>
-            Search
-            <SearchIcon/>
-        </Button>
-    )
-} */
-
-export default class Search extends React.Component<{}, {value: string}> {
+export default class Search extends React.Component<{}> {
 
     public state: any = {
-        value: ""
+        query: "",
+        rated: "",
+        type: "",
+
+        page: 1,
+        status: "",
+
+        genre: 0,
+
     }
 
+    // Searches to myanimelist via the jikan API must be at least 3 characters long, so this just returns true or false
+    // depending on whether the text input is long enough 
+    public CheckInput = () => {
+        if (this.state.query.length >= 3) {
+            return true
+        }
+        return false 
+    }
+
+    // Submits a request to the jikan API based on the currently selected search options and text input
     public SubmitRequest = () => {
-        fetch(`https://api.jikan.moe/search/anime/${this.state.value}/1`)
-        .then(
-            (response: any) => { 
-                if (response.status !== 200) {
-                    console.log('statusCode', response.status)
-                    return
-                }
-                
-                response.json()
-                    .then(
-                        (data: any) => {
-                            console.log('data:', data)
-                    });
-        });
+
+        if (!this.CheckInput()) {
+            console.log("Query < 3")
+            return
+        }
+
+        // Encode the query into a valid url component
+        const query: string = encodeURIComponent(this.state.query)
+
+        fetch(`https://api.jikan.moe/search/anime/${query}/1`)
+            .then(
+                (response: any) => { 
+                    if (response.status !== 200) {
+                        if (response.status === 500) {
+                            console.log('No results found')
+                            return
+                        }
+                        console.log('statusCode', response.status)
+                        return
+                    }
+                    
+                    response.json()
+                        .then(
+                            (data: any) => {
+                                console.log('data:', data)
+                        });
+            });
     }
 
     public handleKeyDown = (event: any) => {
@@ -42,7 +64,7 @@ export default class Search extends React.Component<{}, {value: string}> {
 
     public handleChange = (event: any) => {
         this.setState({ 
-            value: event.target.value 
+            query: event.target.value 
         });
     }
 
@@ -58,7 +80,7 @@ export default class Search extends React.Component<{}, {value: string}> {
                     autoFocus={true} 
                     label="Label" 
                     helperText="helperText"
-                    value={this.state.value}
+                    value={this.state.query}
                     onKeyDown={this.handleKeyDown}
                     onChange={this.handleChange}
                 />
