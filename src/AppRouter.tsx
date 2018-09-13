eb491bd98af6f5302453daeda3df8339430c11eb
Feras Albaroudi <feras.albaroudi@gmail.com>
2018-09-13 19:57:26 +1200

    fixed infinite redirect by adding home page

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 import App from './App';
+import Home from './components/Home';
 import TileResults from './components/TileResults';
 
 // Using a key to force TileResults to remount when the URL changes
@@ -11,17 +12,17 @@ const Result = () => {
     )
 }
 
-
 export const AppRouter: React.StatelessComponent<{}> = () => {
 
     return (
 
         <BrowserRouter>
             <main>     
-                <Route component={App} />      
-                <Switch>                    
+                <App />      
+                <Switch>
+                    <Route exact={true} path='/' component={Home} />                    
                     <Route path='/search/:searchCategory' render={Result}/>
-                    <Redirect exact={true} to='/' />                  
+                    <Redirect from="*" to='/' />                  
                 </Switch>
             </main>
         </BrowserRouter>
---
+++ src/components/Home.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+// The home page is just a larger search box. Nothing special.
+// At the moment this just exists to prevent an infinite redirect in AppRouter
+
+class Home extends React.Component<{}> {
+
+    public render() {
+        return (
+            null
+        )
+    }
+}
+
+export default Home
